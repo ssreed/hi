a1825817f88cf0bba9d11737aefcb7f8bb2444a6
Sri <srisreed@gmail.com>
2013-07-30 23:00:41 -0700

    ver 0.1 updated style, added sticky nav

--- js/app.js
+++ js/app.js
@@ -1,9 +1,43 @@
 $(function(){
 
     function Hi() {
-        console.log('hello');
+        var app = this;
+        app.icon = $('.icon');
+        app.window = $(window);
+        app.navigation = $('.main-navigation');
+        app.offset = app.navigation.offset().top;
     }
 
-    var app = new Hi();
+    Hi.prototype.init = function() {
+        console.log('App initialized!');
+        app.menu();
+    }
 
+    Hi.prototype.menu = function() {
+
+        app.icon.on('mouseover', function(){
+            
+        });
+
+        app.window.on('scroll', function(){
+            if(app.window.scrollTop() > app.offset) {
+                app.navigation.addClass('stuck');
+            } else {
+                app.navigation.removeClass('stuck');
+            }
+        });
+    }
+
+
+    // Hi.prototype.hideDiv = function() {
+    //     console.log(app.window.width());
+    //     if(app.window.width() < 800) {
+    //         app.icon.hide();
+    //     } else {
+    //         app.icon.show();
+    //     }
+    // }
+
+    var app = new Hi();
+        app.init();
 });
